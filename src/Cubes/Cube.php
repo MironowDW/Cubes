316a80e49caa915545bf4a92ffcf53af6ddee1f5
mironovdv <MironovDV@bk.ru>
2014-03-13 23:07:19 +0400

    Refactoring

--- src/Cubes/Cube.php
+++ src/Cubes/Cube.php
@@ -21,20 +21,9 @@ class Cube
         $this->y2 = $y2;
     }
 
-
-    public function setKey($key)
-    {
-        $this->key = $key;
-    }
-
     public function getKey()
     {
         return $this->key;
-    }
-
-    public function setX1($x1)
-    {
-        $this->x1 = $x1;
     }
 
     public function getX1()
@@ -42,29 +31,14 @@ class Cube
         return $this->x1;
     }
 
-    public function setX2($x2)
-    {
-        $this->x2 = $x2;
-    }
-
     public function getX2()
     {
         return $this->x2;
     }
 
-    public function setY1($y1)
-    {
-        $this->y1 = $y1;
-    }
-
     public function getY1()
     {
         return $this->y1;
-    }
-
-    public function setY2($y2)
-    {
-        $this->y2 = $y2;
     }
 
     public function getY2()
